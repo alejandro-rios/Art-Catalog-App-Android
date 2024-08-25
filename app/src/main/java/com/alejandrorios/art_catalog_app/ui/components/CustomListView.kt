10d5d817d728d24788b12822c267b3bda80a6605
Alejandro Rios <alejandrorios77@gmail.com>
2024-08-25 18:19:11 -0500

    - BackToTop composable implemented.

--- app/src/main/java/com/alejandrorios/art_catalog_app/ui/components/CustomListView.kt
+++ app/src/main/java/com/alejandrorios/art_catalog_app/ui/components/CustomListView.kt
@@ -1,7 +1,12 @@
 package com.alejandrorios.art_catalog_app.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -12,7 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -21,6 +29,7 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.unit.dp
 import com.alejandrorios.art_catalog_app.domain.models.Artwork
+import kotlinx.coroutines.launch
 
 /**
  * This composable works as a simple list, the main feature is the pagination option
@@ -88,44 +97,87 @@ private fun CustomListView(
     semanticTag: String = "artworkList",
 ) {
     val listState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
     val reachedBottom: Boolean by remember { derivedStateOf { listState.reachedBottom() } }
 
-    LazyColumn(
-        state = listState,
-        contentPadding = PaddingValues(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(10.dp),
-        modifier = Modifier.semantics { testTagsAsResourceId = true }.testTag(semanticTag)
-    ) {
-        itemsIndexed(items) { _, item: Artwork ->
-            if (onDeleteItem != null) {
-                SwipeToDeleteContainer(item = item, onDelete = { onDeleteItem.invoke(it) }) {
+    LaunchedEffect(reachedBottom) {
+        if (reachedBottom && onNewItems != null) onNewItems.invoke()
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier
+                .semantics { testTagsAsResourceId = true }
+                .testTag(semanticTag)
+        ) {
+            itemsIndexed(items) { _, item: Artwork ->
+                if (onDeleteItem != null) {
+                    SwipeToDeleteContainer(item = item, onDelete = { onDeleteItem.invoke(it) }) {
+                        ArtworkTile(artwork = item) {
+                            onClickItem(item.id)
+                        }
+                    }
+                } else {
                     ArtworkTile(artwork = item) {
                         onClickItem(item.id)
                     }
                 }
-            } else {
-                ArtworkTile(artwork = item) {
-                    onClickItem(item.id)
-                }
+            }
+
+            if (isLoadingItems) item {
+                CircularProgressIndicator(modifier = Modifier.padding(vertical = 16.dp))
             }
         }
 
-        if (isLoadingItems) item {
-            CircularProgressIndicator(modifier = Modifier.padding(vertical = 16.dp))
+        AnimatedVisibility(
+            visible = !listState.isScrollingUp(),
+            enter = fadeIn(),
+            exit = fadeOut(),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+        ) {
+            BackToTop {
+                scope.launch {
+                    listState.scrollToItem(0)
+                }
+            }
         }
-    }
-
-    LaunchedEffect(reachedBottom) {
-        if (reachedBottom && onNewItems != null) onNewItems.invoke()
     }
 }
 
 /***
  * Resource:
- * https://medium.com/@giorgos.patronas1/endless-scrolling-in-android-with-jetpack-compose-af1f55a03d1a
+ * [Endless Scrolling in Android with Jetpack Compose](https://medium.com/@giorgos.patronas1/endless-scrolling-in-android-with-jetpack-compose-af1f55a03d1a)
  */
 internal fun LazyListState.reachedBottom(buffer: Int = 1): Boolean {
     val lastVisibleItem = this.layoutInfo.visibleItemsInfo.lastOrNull()
     return lastVisibleItem?.index != 0 && lastVisibleItem?.index == this.layoutInfo.totalItemsCount - buffer
+}
+
+/**
+ * Resource:
+ * [How to create a jump to top feature with jetpack compose](https://medium.com/@gsaillen95/how-to-create-a-jump-to-top-feature-with-jetpack-compose-2ed487b30087)
+ */
+@Composable
+fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
+
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
 }
